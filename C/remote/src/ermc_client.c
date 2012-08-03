f83882d3c843cc058bbab35f6da7dea1dcd83d49
PrinceAMD.elive@gmail.com <PrinceAMD.elive@gmail.com@7925d7f0-66b0-eafc-3d91-e6a0c10cb699>
2012-08-03 21:27:39 +0000

    one step closer to clean code

--- C/remote/src/ermc_client.c
+++ C/remote/src/ermc_client.c
@@ -1,3 +1,4 @@
+#include "../config.h"
 #include <E_DBus.h>
 #include <Ecore_Getopt.h>
 #define INT 0
@@ -14,7 +15,7 @@ typedef enum _Input_Type
 	INPUT_TYPE_DOUBLE,
 	INPUT_TYPE_STRING,
 	INPUT_TYPE_FLOAT
-	
+
 } Input_Type;
 
 struct _remote_client_vars
@@ -92,10 +93,10 @@ print_iter (DBusMessageIter *iter, int depth)
 	do
 	{
 		int type = dbus_message_iter_get_arg_type (iter);
-	
+
 		if (type == DBUS_TYPE_INVALID)
 			break;
-		
+
 		switch (type)
 		{
 			case DBUS_TYPE_STRING:
@@ -107,7 +108,7 @@ print_iter (DBusMessageIter *iter, int depth)
 				printf ("\"\n");
 				break;
 			}
-			
+
 			case DBUS_TYPE_INT32:
 			{
 				dbus_int32_t val;
@@ -115,7 +116,7 @@ print_iter (DBusMessageIter *iter, int depth)
 				printf ("int32 %d\n", val);
 				break;
 			}
-			
+
 			case DBUS_TYPE_DOUBLE:
 			{
 				double val;
@@ -123,12 +124,12 @@ print_iter (DBusMessageIter *iter, int depth)
 				printf ("double %g\n", val);
 				break;
 			}
-			
+
 			case DBUS_TYPE_ARRAY:
 			{
 				int current_type;
 				DBusMessageIter subiter;
-	
+
 				dbus_message_iter_recurse (iter, &subiter);
 
 				printf("array [\n");
@@ -147,16 +148,16 @@ print_iter (DBusMessageIter *iter, int depth)
 	while (dbus_message_iter_next (iter));
 }
 void
-cb_reply(void *data, DBusMessage *reply, DBusError *error)
+cb_reply(void *data, DBusMessage *reply, DBusError *error __UNUSED__)
 {
 	const char *val;
 	char *array;
-	
+
 	int current_type;
 	DBusMessageIter subiter;
 	DBusMessageIter iter;
 	dbus_message_iter_init (reply, &iter);
-	
+
 	print_iter(&iter,1);
 
 	ecore_main_loop_quit();
@@ -168,13 +169,13 @@ _message(E_DBus_Connection *conn)
 
 	DBusMessage *msg;
 	DBusMessageIter iter;
-	
+
 	msg = dbus_message_new_method_call(
 		"org.enlightenment.Remote.service",
 		"/org/enlightenment/Remote/RemoteObject",
 		config->dbus_obj,config->dbus_name
 	);
-  
+
 	/*
 	printf("Val[0]:%s\n",config->val[0]);
 	printf("Val[1]:%s\n",config->val[1]);
@@ -183,19 +184,19 @@ _message(E_DBus_Connection *conn)
 	printf("Val[4]:%s\n",config->val[4]);
 	printf("Val[5]:%s\n",config->val[5]);
 */
-	
+
 	dbus_message_iter_init_append(msg, &iter);
-	
+
 	int v,w,i=0;
 	int ival;
 	double dval;
-	
+
 	//How many Intergers , Strings , Doubles
 	switch (input_type)
 	  {
 		  case INPUT_TYPE_NONE:
 			  break;
-		  case 
+		  case
 			  INPUT_TYPE_INT:
 			printf("TYPE:INT\n");
 			for (i = 0 ; i < config->vals[INT]; i++)
@@ -223,7 +224,7 @@ _message(E_DBus_Connection *conn)
 			}
 			  break;
 	  }
-	
+
 	printf("i'm at the end\n");
 	e_dbus_message_send(conn, msg, cb_reply, 10, NULL);
 	dbus_message_unref(msg);
@@ -233,17 +234,17 @@ _message(E_DBus_Connection *conn)
 
 /*dbus functions end*/
 
-const Ecore_Getopt general_opts = 
+const Ecore_Getopt general_opts =
 {
 	"enlightenment_remote_module_client",
 	NULL,
-	"0.2",
-	"(C) 2009-2011 - PrinceAMD <princeamd.elive@gmail.com>",
+	PACKAGE_VERSION,
+	"(C) 2009-2012 - PrinceAMD <princeamd.elive@gmail.com>",
 	"GPL v3 - GNU General Public License",
 	"Remote module Client\n"
 	"\n"
 	"These are options which you can use \n "
-	"ERMC Version 0.2",
+	"ermc",
 	0,
 	{
 		ECORE_GETOPT_COUNT
@@ -254,10 +255,10 @@ const Ecore_Getopt general_opts =
 		('\0', "default-bg-set", "  OPT1 Set the default background edje to the desktop \n"
 			"background in the file 'OPT1' (must be a full path)"),
 		ECORE_GETOPT_STORE_STR
-		('\0', "default-bg-add", "OPT1 OPT2 OPT3 OPT4 OPT5 Add a desktop bg definition. \n" 
+		('\0', "default-bg-add", "OPT1 OPT2 OPT3 OPT4 OPT5 Add a desktop bg definition. \n"
 			"OPT1 = container no. OPT2 = zone no. OPT3 = desk_x. OPT4 = desk_y. OPT5 = bg file path"),
 		ECORE_GETOPT_STORE_STR
-		('\0', "default-bg-del", "OPT1 OPT2 OPT3 OPT4 Delete a desktop bg definition.\n " 
+		('\0', "default-bg-del", "OPT1 OPT2 OPT3 OPT4 Delete a desktop bg definition.\n "
 			"OPT1 = container no. OPT2 = zone no. OPT3 = desk_x. OPT4 = desk_y."),
 		ECORE_GETOPT_STORE_STR ('\0', "bg-set-this-screen", "Set a specific wallpaper on this screen"),
 		ECORE_GETOPT_STORE_STR ('\0', "bg-set-current-desktop", "Set a specific wllpaper on this desktop alone"),
@@ -313,7 +314,7 @@ _args_init(int argc, char **argv)
 {
 	config = calloc(1, sizeof(Remote_Client_Vars));
 	int arg_index, i,run;
-	
+
 	Ecore_Getopt_Value general_values[] = {
 		ECORE_GETOPT_VALUE_BOOL(config->null),
 		ECORE_GETOPT_VALUE_BOOL(config->null),
@@ -367,15 +368,15 @@ _args_init(int argc, char **argv)
 		ECORE_GETOPT_VALUE_BOOL(config->quit_bool),
 		ECORE_GETOPT_VALUE_NONE
 	};
-	
+
 	if(!config->null)
 		arg_index = ecore_getopt_parse(&general_opts, general_values, argc, argv);
 	else if(config->quit_bool)
 		arg_index = ecore_getopt_parse(&general_opts, general_values, argc, argv);
-	
+
 	run = 1;
 	i = 0;
-	
+
 	while(run)
 	{
 		if(!values[i][0])
@@ -387,10 +388,10 @@ _args_init(int argc, char **argv)
 			if(!strcmp(argv[1], values[i][0]))
 			{
 				int v,w,x=0;
-				
+
 				config->dbus_obj = eina_stringshare_add(values[i][1]);
 				config->dbus_name = eina_stringshare_add(values[i][2]);
-				
+
 				//How many Intergers , Strings , Doubles
 				input_type = INPUT_TYPE_NONE;
 				for (w = 3; w <= 5; w++)
@@ -404,7 +405,7 @@ _args_init(int argc, char **argv)
 							input_type = INPUT_TYPE_STRING;
 						else if ( x == DOUBLE )
 							input_type = INPUT_TYPE_DOUBLE;
-						
+
 						for (v = 0; v < config->vals[x]; v++)
 						{
 							config->val[v] = argv[2+v];
@@ -414,12 +415,12 @@ _args_init(int argc, char **argv)
 			}
 		}i++;
 	}
-	
+
 	if(config->quit_bool)
 	{
 		ecore_main_loop_quit();
 	}
-	
+
 return EINA_TRUE;
 }
 
@@ -428,16 +429,16 @@ int main(int argc, char **argv)
 	ecore_init();
 	e_dbus_init();
 	E_DBus_Connection *conn;
-	
+
 	conn = e_dbus_bus_get(DBUS_BUS_SESSION);
-	
+
 	_args_init(argc, argv);
-	
+
 	if((argc > 1) && (!config->quit_bool))
 		_message(conn);
 	else
 	ecore_main_loop_quit();
-	
+
 	ecore_main_loop_begin();
 	ecore_shutdown();
 }
